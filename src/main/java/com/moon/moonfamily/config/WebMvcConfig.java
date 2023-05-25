a1a3458b296d6407628a220826d9d16427685815
MooN <rankupkor@gmail.com>
2023-05-25 21:43:59 +0900

    클래스명 변경

--- src/main/java/com/moon/moonfamily/config/WebMvcConfig.java
+++ src/main/java/com/moon/moonfamily/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/profile-pictures/**").addResourceLocations("file:/home/ubuntu/spring/profile-pictures/");
